285925ebb81346004964d1f6a614a543d40d490f
usmanghaniofficial <usmanghani110099@gmail.com>
2025-08-21 16:55:33 +0500

    While loop

---
+++ for loop/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+                                                                  //int i=1;
+                                                                  //while(i<=5)
+                                                                  //{cout<< i;
+                                                                  //i++;}
+{
+    int i;
+    int num[]={1,7,8,9,3,2};
+   for(i ; i<=5 ; i++)
+{
+       cout<<num[i]<<"\n";
+}
+   cout<<num[3];
+    return 0;
+}
